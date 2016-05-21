ac6367b11e84be4b3597ed9bc279de9e89d49888
Ashwin Rao <theonlyrao@gmail.com>
2016-05-21 11:12:06 -0600

    Use gsub to solve general problem
    This commit uses gsub to replae the opening and closing parentheses. It
    is not as elegant an implementation as we thought, since the string "-1"
    gets split by chars.
    
    As a result the reduce block requires additional work before it can
    properly calculate the total.
    
    I think it would be better to leave it as chars and then use flow
    control to assign the correct value to n within the block, but I
    implemented the solution anyway for your review.

--- day_one_test.rb
+++ day_one_test.rb
@@ -2,19 +2,26 @@ require "minitest/autorun"
 
 class DayOne
   def self.get_floor(direction, floor)
-    direction.chars.reduce(floor) do |delta, n|
-      if n == "("
-        movement = 1
-      else
-        movement = -1
-      end
-      delta += movement
+    direction.gsub(/[()]/, "(" => "1", ")" => "-1").chars.reduce(floor) do |delta, n|
+      # if n == "("
+      #   movement = 1
+      # else
+      #   movement = -1
+      # end
+      delta -= 2 if n.to_i == 0 # this craziness because chars splits the - from the 1
+      delta += n.to_i
     end
   end
 end
 
 
 class DayOneTest < Minitest::Test
+  def test_we_can_move_more_than_one_floor_and_direction
+    current_floor = 0
+    new_floor = DayOne.get_floor("(()())())", current_floor)
+    assert_equal -1, new_floor
+  end
+
   def test_we_can_move_more_than_one_floor
     current_floor = 0
     new_floor = DayOne.get_floor("((", current_floor)
